209f5a6f7f32cd3d50868c60fb6827ff3b74064d
Rémino Rem <29999+remino@users.noreply.github.com>
2023-04-25 16:58:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-export { default as isBetween } from './js/is-between.js'
+export { default as isBetween } from './lib/is-between.js'
+export * as jsClass from './lib/js-class.js'
+export * as template from './lib/template.js'
