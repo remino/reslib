c99bb4f8802817a5773d9e6c8d2bf3b7d66a8fee
Rémino Rem <29999+remino@users.noreply.github.com>
2023-10-10 07:10:20 +0900

    Add dig()

---
+++ spec/lib/dig.spec.js
@@ -0,0 +1,33 @@
+import dig from '../../lib/dig.js'
+
+describe('lib/dig.js', () => {
+	describe('dig()', () => {
+		it('returns undefined if value is not found', () => {
+			expect(dig({}, 'a')).toBeUndefined()
+		})
+
+		it('returns undefined if parent value is not an object', () => {
+			expect(dig({ a: 'b' }, 'a.b')).toBeUndefined()
+		})
+
+		it('returns value from object', () => {
+			expect(dig({ a: { b: { c: 'd' } } }, 'a.b.c')).toBe('d')
+		})
+
+		it('sets value on object', () => {
+			const object = {}
+			dig(object, 'a.b.c', 'e')
+			expect(object).toEqual({ a: { b: { c: 'e' } } })
+		})
+
+		it('throws error if parent value is not an object', () => {
+			expect(() => dig({ a: 'b' }, 'a.b.c', 'd')).toThrow()
+		})
+
+		it('does not create object if value is undefined', () => {
+			const object = {}
+			dig(object, 'a.b.c')
+			expect(object).toEqual({})
+		})
+	})
+})
