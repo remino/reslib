6a59e39186b53c29de86aeb9cdaacdb8359761bf
Rémino Rem <29999+remino@users.noreply.github.com>
2025-10-04 19:44:00 +0900

    Add files built by tsc

--- lib/middleware/proxy-files.d.ts
+++ lib/middleware/proxy-files.d.ts
@@ -1,15 +1,10 @@
-import type { MiddlewareNext } from './chain'
-type ProxyFilesRecord = Record<string, string>
+import type { MiddlewareNext } from './chain';
+type ProxyFilesRecord = Record<string, string>;
 type ProxyFilesObject = {
-	paths: ProxyFilesRecord
-}
-declare const proxyFiles: (config: ProxyFilesObject) => (
-	{
-		url,
-	}: {
-		url: URL
-	},
-	next: MiddlewareNext,
-) => Promise<void | Response>
-export default proxyFiles
+    paths: ProxyFilesRecord;
+};
+declare const proxyFiles: (config: ProxyFilesObject) => ({ url }: {
+    url: URL;
+}, next: MiddlewareNext) => Promise<void | Response>;
+export default proxyFiles;
 //# sourceMappingURL=proxy-files.d.ts.map
---
+++ lib/url.d.ts
@@ -0,0 +1,6 @@
+export declare function makeAbsolute(base: string, path?: string): string;
+declare const _default: {
+    makeAbsolute: typeof makeAbsolute;
+};
+export default _default;
+//# sourceMappingURL=url.d.ts.map
