4b549c705641e24a5695a3828411e9fc5201f0ff
Rémino Rem <29999+remino@users.noreply.github.com>
2025-10-04 19:42:52 +0900

    Format lib/url.ts

--- lib/url.ts
+++ lib/url.ts
@@ -1,6 +1,5 @@
 export function makeAbsolute(base: string, path = ''): string {
-  return new URL(path.replace(/^\/+/, '/'), base).toString()
+	return new URL(path.replace(/^\/+/, '/'), base).toString()
 }
 
 export default { makeAbsolute }
-
