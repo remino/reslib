76c2cefeda9207487ea533d8ecc158711dce8880
Rémino Rem <29999+remino@users.noreply.github.com>
2025-11-29 13:24:44 +0900

    Adjust postcss-plugins exports

--- packages/postcss-plugins/src/index.ts
+++ packages/postcss-plugins/src/index.ts
@@ -1,3 +1,3 @@
-export { default as addDvh } from './addDvh'
-export { default as addLvh } from './addLvh'
-export { default as addFontVariation } from './addFontVariation'
+export { default as addDvh } from './addDvh.js'
+export { default as addLvh } from './addLvh.js'
+export { default as addFontVariation } from './addFontVariation.js'
